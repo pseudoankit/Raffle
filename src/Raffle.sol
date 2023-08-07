a61c7cdc2be5ee27c55b4b8e3c6fb2ee0061a3b8
ankitkumar <lostankit7@gmail.com>
2023-08-07 06:43:47 +0530

    added method to trigger pick winner automatically via chainlink

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -8,9 +8,18 @@ contract Raffle is VRFConsumerBaseV2 {
     error Raffle__NotEnoughEthSent();
     error Raffle__TransferFailed();
     error Raffle_NotOpened();
+    error Raffle_UpKeyNotNeeded(
+        uint256 currentBalance,
+        uint256 numPlayers,
+        RaffleState raffleState
+    );
 
     /**Type Declaration */
-    enum RaffleState {Open, Closed, Calculating}
+    enum RaffleState {
+        Open,
+        Closed,
+        Calculating
+    }
 
     /**State variables */
     uint16 private constant REQUEST_CONFIRMATIONS = 3;
@@ -33,7 +42,7 @@ contract Raffle is VRFConsumerBaseV2 {
     event WinnerPicked(address indexed winner);
 
     constructor(
-        uint256 _entrancefee, 
+        uint256 _entrancefee,
         uint256 _interval,
         address _vrfCoordinator,
         bytes32 _gasLane,
@@ -51,11 +60,11 @@ contract Raffle is VRFConsumerBaseV2 {
     }
 
     function enterRaffle() external payable {
-        if(msg.value < i_entranceFee) {
+        if (msg.value < i_entranceFee) {
             revert Raffle__NotEnoughEthSent();
         }
 
-        if(s_state != RaffleState.Open) {
+        if (s_state != RaffleState.Open) {
             revert Raffle_NotOpened();
         }
 
@@ -63,14 +72,35 @@ contract Raffle is VRFConsumerBaseV2 {
         emit EnteredRaffle(msg.sender);
     }
 
-    function pickWinner() external {
-        if((block.timestamp - s_lastTimeStamp) < i_interval) {
-            revert();
+    /**
+     * @dev Function that chainlink automation nodes call to see if it's time to perform an upKey
+     * 
+     */
+    function checkUpKey(
+        bytes memory /*checkData*/
+    ) public view returns (bool upKeyNeeded, bytes memory performData) {
+        bool timeHasPassed = (block.timestamp - s_lastTimeStamp) >= i_interval;
+        bool isOpen = s_state == RaffleState.Open;
+        bool hasBalance = address(this).balance > 0;
+        bool hasPlayer = s_players.length > 0;
+
+        upKeyNeeded = (timeHasPassed && isOpen && hasBalance && hasPlayer);
+        return (upKeyNeeded, "0x0");
+    }
+
+    function peformUpKey(bytes memory /*peformData*/) external {
+        (bool upKeyNeeded,) = checkUpKey("");
+        if (!upKeyNeeded) {
+            revert Raffle_UpKeyNotNeeded(
+                address(this).balance,
+                s_players.length,
+                s_state
+            );
         }
 
         s_state = RaffleState.Calculating;
 
-        uint256 requestId = i_vrfCoordinator.requestRandomWords(
+        i_vrfCoordinator.requestRandomWords(
             i_gasLane,
             i_subscriptionId,
             REQUEST_CONFIRMATIONS,
@@ -80,7 +110,7 @@ contract Raffle is VRFConsumerBaseV2 {
     }
 
     function fulfillRandomWords(
-        uint256 requestId,
+        uint256 /*requestId*/,
         uint256[] memory randomWords
     ) internal override {
         uint256 indexOfWinner = randomWords[0] % s_players.length;
@@ -89,19 +119,19 @@ contract Raffle is VRFConsumerBaseV2 {
         s_recentWinner = winner;
         s_state = RaffleState.Open;
         s_players = new address payable[](0);
-        s_lastTimeStamp = block.timestamp();
-
-        (bool success, ) = winner.call{value: address(this).balance}("");
-        if(!success) {
-            revert Raffle__TransferFailed();
-        }
+        s_lastTimeStamp = block.timestamp;
 
         emit WinnerPicked(s_recentWinner);
+
+        (bool success, ) = winner.call{value: address(this).balance}("");
+        if (!success) {
+            revert Raffle__TransferFailed();
+        }
     }
 
     /** Getters */
 
-    function getEntranceFee() external view returns(uint256) {
+    function getEntranceFee() external view returns (uint256) {
         return i_entranceFee;
     }
 }
